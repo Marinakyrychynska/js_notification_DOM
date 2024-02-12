ca1da5256107f05ad27bbe85aecef3db198eb630
Maryna Kyrychynska <m_kirichok@ukr.net>
2024-02-12 16:44:37 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,25 @@
+"use strict";
+const pushNotification = (posTop, posRight, title, description, type)=>{
+    const body = document.querySelector("body");
+    const message = document.createElement("div");
+    message.className = "notification";
+    message.classList.add(type);
+    message.style.top = `${posTop}px`;
+    message.style.right = `${posRight}px`;
+    const titleMessage = document.createElement("h2");
+    titleMessage.className = "title";
+    titleMessage.textContent = title;
+    const descriptionMessage = document.createElement("p");
+    descriptionMessage.innerHTML = description;
+    message.append(titleMessage);
+    message.append(descriptionMessage);
+    body.append(message);
+    setTimeout(()=>{
+        message.remove();
+    }, 2000);
+};
+pushNotification(10, 10, "Title of Success message", "Message example.\n Notification should contain title and description.", "success");
+pushNotification(150, 10, "Title of Error message", "Message example.\n Notification should contain title and description.", "error");
+pushNotification(290, 10, "Title of Warning message", "Message example.\n Notification should contain title and description.", "warning");
+
+//# sourceMappingURL=index.f75de5e1.js.map
